d2bcecf12a8c41fecc1e045c49d7ca0a7cea1e03
Laguna1 <lagunazoomarket@gmail.com>
2020-05-18 19:46:47 +0300

    add validation method

--- bin/main.rb
+++ bin/main.rb
@@ -1,88 +1,122 @@
 def welcome_message
-    puts 'Welcome to Tic Tac Toe Game'
+  puts 'Welcome to Tic Tac Toe Game'
   end
-  welcome_message
-  
-  def user_setup
-    print 'Please Enter Player One name '
-    player1 = gets.chomp
-    print 'Please Enter Player Second name '
-    player2 = gets.chomp
-  
-    print "Choose #{player1} X or O: "
+welcome_message
+
+def user_setup
+  print 'Please Enter Player One name '
+  player1 = gets.chomp
+  player1.upcase!
+  print 'Please Enter Player Second name '
+  player2 = gets.chomp
+  player2.upcase!
+
+    chosen = true
+    while chosen 
+    print "Choose #{player1} 'X' or 'O': "
     mark = gets.chomp
     mark.upcase!
     if mark == 'X'
-      puts "X is assigned to #{player1} and O is assigned to #{player2}"
+      chosen = false
+      puts "'X' is assigned to #{player1} and 'O' is assigned to #{player2}"
+    elsif mark =='O'
+      chosen = false
+      puts "'O' is assigned to #{player1} and 'X' is assigned to #{player2}"
     else
-      puts "O is assigned to #{player1} and X is assigned to #{player2}"
+    
+      puts "Your choise is invalid. Please chhoose again."
     end
   end
-  
-  user_setup
-  
-  puts 'Game Board'
-  board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-  def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+                   
+end
+
+user_setup
+
+puts 'Game Board'
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']      
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts '-----------'
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts '-----------'
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def board_example
+
+    puts " 1 | 2 | 3 "
     puts '-----------'
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " 4 | 5 | 6 "
     puts '-----------'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " 7 | 8 | 9 "
+
+end
+
+def empty_index_board(board, index)
+  if board[index] == ' ' || board[index] == 'X' || board[index] == 'O'
+    true
+  else
+    false
   end
-  display_board(board)
-  
-  def empty_index_board(board, index)
-    if board[index] == ' ' || board[index] == 'X' || board[index] == 'O'
-      true
-    else
-      false
-    end
-  end
+end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
-
-def choose_message
-    print 'Please choose number from 1 to 9: '
+def move(board, index, player)
+  board[index] = player
 end
 
 
-def choose_num1(board)                        
-    arr = []
+def position_taken?(board, index)
+  board[index]== "X" || board[index] == "O"
+end
 
-    choose_message
-     condition = true
-    while condition
-  
-      number1 = gets.to_i
-      if board[number1] == " " || board[number1] == "" || board[number1] == nil 
-        condition = false
-        board[number1 - 1] = 'X'
-        display_board(board)
-      else
-        print 'Enter valid number: '
-      end
-  end
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def choose_message
+  print 'Please choose number from 1 to 9: '
+end
+board_example                       
+def choose_num1(board)
   choose_message
+    number1 = gets.to_i
+    index = input_to_index(number1)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    print "Invalid Number. "
+    choose_num1(board)
+  end
+end
 
-   condition2 = true
-    while condition2
-  
-      number2 = gets.to_i
-      if board[number2] == " " || board[number2] == "" || board[number2] == nil 
-        condition2 = false
-        board[number2 - 1] = 'O'
-        display_board(board)
-      else
-        print 'Enter valid number: '
-      end
-  end
-  end
-  choose_num1(board)
-  choose_num1(board)
-  choose_num1(board)
-  choose_num1(board)
- 
-  
-  
+choose_num1(board)
+choose_num1(board)
+choose_num1(board)
+choose_num1(board)
+choose_num1(board)
+choose_num1(board)
+choose_num1(board)
+choose_num1(board)
+choose_num1(board)
+
+
+
+def player_winner            
+end
+
+puts "Game over"             
+puts "#{player_winner} won"  
+puts "VWant to play more?"  
